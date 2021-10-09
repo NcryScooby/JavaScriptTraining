a55f6cb60b382e12d9002cadbe04a39cee7b796e
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-10-09 18:13:52 -0300

    #7

---
+++ ternario/script.js
@@ -0,0 +1,9 @@
+let nota;
+
+function calcularNota (nota){
+    resultado = nota >= 6 ? 'Aprovado' : 'Reprovado';
+    return resultado; 
+}
+
+console.log(calcularNota(6));
+console.log(resultado);
