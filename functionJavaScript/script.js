3cf7ebfcaefbb143382262e882363b2519f42ebf
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-10-09 15:37:51 -0300

    #4

--- functionJavaScript/script.js
+++ functionJavaScript/script.js
@@ -33,3 +33,23 @@ function trocaCor(cor, tonalidade){
 trocaCor("verde", " fraco");
 
 console.log(corLapis);
+
+////////////////////////////////////////////////////////////////// Tipos de Funções
+
+//Realizar uma tarefa, não devolve nada
+function dizerNome(){
+    console.log('Fabricio');
+}
+
+dizerNome();
+
+//Faz um calculo e operação e retorna algo
+function multiplicarPorDois(valor){
+    return valor * 2;
+}
+
+//console.log(multiplicarPorDois(5));
+
+let resultado = (multiplicarPorDois(5));
+
+console.log(resultado);
