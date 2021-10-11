8dd746501036130f179308a86bd40f3208f91409
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-10-11 20:23:37 -0300

    #11

---
+++ horaAtual/script.js
@@ -0,0 +1,9 @@
+let d = new Date();
+document.body.innerHTML =
+  "<div class=teste><h1>Horário Atual: " +
+  d.getHours() +
+  ":" +
+  d.getMinutes() +
+  ":" +
+  d.getSeconds();
+("</h1> <div/>");
