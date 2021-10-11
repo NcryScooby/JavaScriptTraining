097573b7e7442277ec9964f99b683b011b399eb4
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-10-11 17:49:25 -0300

    #10

---
+++ numbersJavaScript/script.js
@@ -0,0 +1,11 @@
+//Tipos de Numbers
+
+//33 - Inteiros
+//12.5 - Reais / float
+//NaN - Not a Number
+//Infinity - infinito
+
+console.log('Inteiros', 35);
+console.log('Reais', 15.5);
+console.log('NaN', 5 / "a");
+console.log('Infinity', 12.5 === Infinity);
---
+++ stringsJavaScript/script.js
@@ -0,0 +1,8 @@
+//Tipos de String
+
+//""
+//''
+//``
+
+console.log(`Fabrício ${5 + 5}`); //Interpolação
+console.log(5 + 5);
